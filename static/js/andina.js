9ac94e3a150c93783a59230eb6d14abfccec6bd2
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2012-10-03 22:44:27 -0500

    changes

--- static/js/andina.js
+++ static/js/andina.js
@@ -1,16 +1,31 @@
 function andina_news(){        
         var noticias = $("#noticias");
-	$.getJSON('/andina/noticias/json/', function(data){
+	$.getJSON('http://50.116.43.119:8070/andina/noticias/json/?callback=?', function(data){
                 $.each(data, function(key,value){                
 			noticias.append(key +" = " + value["titular"]);
 		});
 		
 	});
 }
+function andina_news2(){
+$.ajax({
+  dataType: 'json',
+  data: 'id=10',
+  jsonp: 'jsonp_callback',
+  url: 'http://50.116.43.119:8070/andina/noticias/json/',
+  success: function () {
+    alert("exito");
+  },
+}).done(function() { alert("success"); })
+    .fail(function(jqXHR, textStatus) { alert("error"+textStatus); })
+    .always(function() { alert("complete"); });
+
+}
+
 /*
 $.each(data, function(key,value){                
 			noticias.append(key);
 		});*/
 $(document).ready(function() {
-andina_news();
+andina_news2();
 });

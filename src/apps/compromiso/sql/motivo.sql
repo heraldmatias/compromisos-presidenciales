ce37c3fa53b5ed6472e3cf10bb0c89cac23d806a
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2013-02-07 22:02:02 -0500

    fixes

---
+++ src/apps/compromiso/sql/motivo.sql
@@ -0,0 +1,3 @@
+insert into compromiso_motivo(nombre) values ('Viaje');
+insert into compromiso_motivo(nombre) values ('Audiencia con el Presidente');
+insert into compromiso_motivo(nombre) values ('Reunión en Oficina');
